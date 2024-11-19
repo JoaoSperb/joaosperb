b13ecd1a7ce98c7940901e6e4bc74ac7b04cc933
Joao Guilherme <lecostela123@gmail.com>
2024-11-19 09:12:26 -0300

    first commit

---
+++ src/main/java/trabalhofinal/joaosperb/JoaosperbApplication.java
@@ -0,0 +1,13 @@
+package trabalhofinal.joaosperb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaosperbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaosperbApplication.class, args);
+	}
+
+}
